56fcd3e094ba0e74214395408ae6020094d63f5a
lan <helan0106@163.com>
2023-04-08 01:01:39 +0800

    feat(config): 添加 unocss、修改其他配置文件

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Vue 3 + TypeScript + Vite
+
+## 技术栈
+
+- Vue3 pinia
--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
+import UnoCss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCss(),
     AutoImport({
       imports: ['vue'],
     }),
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'rc'),
+      '@': resolve(__dirname, 'src'),
     },
   },
   base: './',
